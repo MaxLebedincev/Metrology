b4f153ce57c1acd414616b5c19f3a95b6563bb7b
Максим Лебединцев <dypakin@gmail.com>
2024-02-25 04:14:45 +0400

    added func calculate

--- app/src/main/java/com/example/metrology/MainActivity.java
+++ app/src/main/java/com/example/metrology/MainActivity.java
@@ -22,8 +22,6 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -35,25 +33,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private SensorManager sensorManager;
     private Sensor accleroSensor;
-    private SensorEventListener sv;
+    private SensorEventListener sensorEventView;
+    private SensorEventListener sensorEventReal;
     private LineChart lineChart;
     private List<String> xValues;
-
-
-    private List<Date> counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.calcul);
-        button.setOnClickListener(this);
+        Button btn1 = findViewById(R.id.calcul);
+        Button btn2 = findViewById(R.id.left);
+        Button btn3 = findViewById(R.id.right);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+        btn2.setEnabled(false);
+        btn3.setEnabled(false);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         if (sensorManager != null){
             accleroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-            sv = new SensorEventListener(){
+            sensorEventView = new SensorEventListener(){
                 @Override
                 public void onSensorChanged(SensorEvent event) {
                     if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
@@ -99,10 +102,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         xAxis.setValueFormatter(new IndexAxisValueFormatter(xValues));
         xAxis.setLabelCount(4);
         xAxis.setGranularity(1f);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setAxisMaximum(1000f);
 
         YAxis yAxis = lineChart.getAxisLeft();
-        yAxis.setAxisMinimum(0f);
-        yAxis.setAxisMaximum(100f);
+        yAxis.setAxisMinimum(-13f);
+        yAxis.setAxisMaximum(13f);
         yAxis.setAxisLineWidth(2f);
         yAxis.setAxisLineColor(Color.BLACK);
         yAxis.setLabelCount(10);
@@ -137,30 +142,166 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(sv, accleroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventView, accleroSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(sv);
+        sensorManager.unregisterListener(sensorEventView);
+    }
+
+    List<List<Entry>> entries = new ArrayList<List<Entry>>() {{
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+        add(new ArrayList<>());
+    }};
+
+    static int counterEntries = 0;
+    static int counterButton = 0;
+    static int counterPage = 0;
+
+    public void CalculationGrapics(float x, float y, float z, int[] numbersEntryList, int step) {
+
+        if (counterEntries == (step - 1)) {
+            counterEntries = 0;
+            return;
+        }
+
+        if (counterEntries == 0){
+            entries.set(numbersEntryList[0], new ArrayList<>());
+            entries.set(numbersEntryList[1], new ArrayList<>());
+            entries.set(numbersEntryList[2], new ArrayList<>());
+        }
+
+        if (counterEntries >= 0 && counterEntries < step) {
+            entries.get(numbersEntryList[1]).add(new Entry(counterEntries, x));
+            entries.get(numbersEntryList[2]).add(new Entry(counterEntries, y));
+            entries.get(numbersEntryList[2]).add(new Entry(counterEntries, z));
+        }
+
+        counterEntries++;
+
+        try {
+            TimeUnit.MICROSECONDS.sleep(2000 / step);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.calcul){
 
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            Button btn = (Button) findViewById(R.id.calcul);
+            Button btnRight = findViewById(R.id.right);
+
+            if (sensorManager != null && accleroSensor != null){
+                sensorEventReal = new SensorEventListener(){
+                    @Override
+                    public void onSensorChanged(SensorEvent event) {
+                        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+
+                            float x = (float) Math.round(event.values[0] * 100) / 100;
+                            float y = (float) Math.round(event.values[1] * 100) / 100;
+                            float z = (float) Math.round(event.values[2] * 100) / 100;
+
+                            if (counterEntries == 0) {
+                                Toast.makeText(getApplicationContext(), "Расчет начат.", Toast.LENGTH_SHORT).show();
+                                btn.setEnabled(false);
+                            }
+
+                            if (counterButton == 0) {
+                                CalculationGrapics(x, y, z, new int[] {0, 1, 2}, 10);
+                            }
+                            else if (counterButton == 1) {
+                                CalculationGrapics(x, y, z, new int[] {3, 4, 5}, 100);
+                            }
+                            else if (counterButton == 2) {
+                                CalculationGrapics(x, y, z, new int[] {6, 7, 8}, 1000);
+                            }
+
+                            if (counterEntries == 9 && counterButton == 0 && counterPage == 0){
+                                btnRight.setEnabled(true);
+                            }
+
+                            if (
+                                (counterEntries == 9 && counterButton == 0) ||
+                                (counterEntries == 99 && counterButton == 1) ||
+                                (counterEntries == 999 && counterButton == 2)
+                            ) {
+                                counterButton = (counterButton == 2) ? 0 : ++counterButton;
+                                counterEntries = 0;
+                                sensorManager.unregisterListener(sensorEventReal);
+                                btn.setEnabled(true);
+                                Toast.makeText(getApplicationContext(), "Расчет завершен.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                    @Override
+                    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                    }
+                };
+                sensorManager.registerListener(sensorEventReal, accleroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else {
+                Toast.makeText(this, "Sensor service not detected.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (v.getId() == R.id.left){
+            if (counterPage != 0) {
+                counterPage--;
+
+                if (counterPage == 0){
+                    Button btn = (Button) findViewById(R.id.left);
+                    btn.setEnabled(false);
+                }
+                else if (counterPage == 7) {
+                    Button btn = (Button) findViewById(R.id.right);
+                    btn.setEnabled(true);
+                }
+
+                LineDataSet dataSet = new LineDataSet(entries.get(counterPage), "Science");
+                dataSet.setColor(Color.RED);
+
+                LineData lineData = new LineData(dataSet);
+
+                lineChart.setData(lineData);
+
+                lineChart.invalidate();
             }
 
-            Date date = new Date();
-            counter.add(date);
+        }
+        else if (v.getId() == R.id.right){
 
-//            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//            Toast.makeText(this, dateFormat.format(date), Toast.LENGTH_SHORT).show();
+            if (counterPage != 8) {
+                counterPage++;
+
+                if (counterPage == 8){
+                    Button btn = (Button) findViewById(R.id.right);
+                    btn.setEnabled(false);
+                }
+                else if (counterPage == 1) {
+                    Button btn = (Button) findViewById(R.id.left);
+                    btn.setEnabled(true);
+                }
+
+                LineDataSet dataSet = new LineDataSet(entries.get(counterPage), "Science");
+                dataSet.setColor(Color.RED);
+
+                LineData lineData = new LineData(dataSet);
+
+                lineChart.setData(lineData);
+
+                lineChart.invalidate();
+            }
         }
     }
 }
